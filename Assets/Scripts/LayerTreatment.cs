e3ae93e48a5e87c284214578b0c5876da237c2ae
Owen Beck <112406398+oab1881@users.noreply.github.com>
2024-11-08 13:16:20 -0500

    Commented out LayerTreatment script

--- Assets/Scripts/LayerTreatment.cs
+++ Assets/Scripts/LayerTreatment.cs
@@ -4,80 +4,91 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
+//Script handles treatments for Oven, Freezer and Goop
+//Will use delta time to change the color slowly of the ingredient being treated
+//Written by Jake + Owen
+
 public class LayerTreatment : MonoBehaviour
 {
     //Stores the storage script
     [SerializeField]
-    Storage storageScript;
+    Storage storageScript;  //Reference to Storage
 
-    int currentCount;
+    int currentCount;   //Current count of ingredients being stored
 
-    LayerState changeType;
-    Color changeColor;
+    LayerState changeType;  //Reference to FoodData - LayerState enum
+    Color changeColor;      //Color that the ingredient will change to (Red, Blue, Green)
 
-    List<float> timers = new List<float>();
+    List<float> timers = new List<float>(); //List of timers used to keep track of timers on treated food
 
     private void Start()
     {
-        if(gameObject.name == "oven red")
+        if(gameObject.name == "oven red")   //If ingredient goes in oven
         {
-            changeType = LayerState.Cooked;
-            changeColor = Color.red;
+            changeType = LayerState.Cooked; //Change its state to cooked
+            changeColor = Color.red;        //Change color to red
         }
-        else if(gameObject.name == "freezer blue")
+        else if(gameObject.name == "freezer blue")  //If ingredient goes in freezer
         {
-            changeType = LayerState.Frozen;
-            changeColor = Color.blue;
-        }else if(gameObject.name == "goop green")
-        {
-            changeType = LayerState.Gooped;
-            changeColor = Color.green;
+            changeType = LayerState.Frozen; //Change its state to frozen
+            changeColor = Color.blue;       //Change color to blue
         }
-        currentCount = storageScript.currentCapacity;
+        else if(gameObject.name == "goop green")   //If ingredient goes in goop
+        {
+            changeType = LayerState.Gooped;     //Change state to gooped
+            changeColor = Color.green;          //Change color to Green
+        }
+        currentCount = storageScript.currentCapacity;   //Get current count of ingredients being stored
     }
 
     private void Update()
     {
         /*
-        if(currentCount < storageScript.currentCapacity)
+        if(currentCount < storageScript.currentCapacity)    //If the current # of ingredients being stored < current capacity of objects that can be stored
         {
-            currentCount++;
-            CreateTimer();
+            currentCount++;     //Increase # of ingredients being stored
+            CreateTimer();      //Create a timer for this ingredient
         }
-        DecreaseTimer();
-        CheckTimer();
+        DecreaseTimer();        //Call Decrease timer every frame
+        CheckTimer();           //Call CheckTimer to see if timer ended
         */
     }
 
+    //Create a timer for an instance of a treated food
     private void CreateTimer()
     {
+        //For # of ingredients stored
         for(int i = 0; i < storageScript.currentCapacity; i++)
         {
+            //If the stored ingredient is in it's base state (untreated)
             if (storageScript.StoredItem[i].GetComponent<FoodData>().CurrentState == LayerState.Base)
             {
-                timers.Add(5f);
+                timers.Add(5f); //Create 5 second timer
             }
         }
     }
 
-
+    //Check to see if timer has completed
     private void CheckTimer()
     {
+        //For # of timers
         for(int i = 0; i < timers.Count; i++)
-        {
+        {   
+            //If timer ends
             if (timers[i] < 0.0f)
             {
-                storageScript.StoredItem[i].GetComponent<FoodData>().ChangeState(changeType,changeColor);
-                timers.RemoveAt(i);
+                storageScript.StoredItem[i].GetComponent<FoodData>().ChangeState(changeType,changeColor);  //Change food's type and color
+                timers.RemoveAt(i); //Remove timer from list
             }
         }
     }
 
+    //Loop thru timer using delta time
     private void DecreaseTimer()
     {
         for(int i = 0; i < timers.Count; i++)
         {
-            timers[i] -= Time.deltaTime;
+            timers[i] -= Time.deltaTime;    //Subtract timer's length (5s) by Time.deltaTime
         }
     }
 }
