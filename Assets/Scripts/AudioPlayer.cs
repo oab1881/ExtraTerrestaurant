ff6572a5275521e8e1f150825c3a56c1fc83d08a
Jak3M <jlw6563@rit.edu>
2024-11-11 14:25:47 -0500

    Audio Manager; Audio clips
    Added all the audio clips we have thus far to the sound effects folder and the music folder. Did more work on audio manager

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -1,16 +1,58 @@
+/*
+ ==== Created by Jake Wardell 11/11/24 ====
+
+Plays audio clips both sound effects and music
+
+Attached: Will be attached to the AudioPlayer Gameobject
+
+Changelog:
+    -Created script: 11/11/24 : Jake
+    
+    
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioPlayer : MonoBehaviour
 {
+    //For storing all sound effects
+    //For storing all music clips
     [SerializeField]
-    List<AudioClip> audioClips= new List<AudioClip>();
+    List<AudioClip> soundEffectClip= new List<AudioClip>();
+    List<AudioClip> musicClips = new List<AudioClip>();
 
+    //Stores reference to audio source
+    [SerializeField]
+    AudioSource audioSource;
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Plays a Sound effect by passing the name in
+    /// </summary>
+    /// <param name="name"></param>
+    public void PlaySoundEffect(string name)
     {
-        
+        for(int i = 0; i < soundEffectClip.Count; i++)
+        {
+            if(name == soundEffectClip[i].name)
+            {
+                audioSource.PlayOneShot(soundEffectClip[i]);
+                break;
+            }
+        }
+    }
+
+    public void PlayMusic(string name, bool loop)
+    {
+        /*
+        for(int i = 0; i < musicClips.Count; i++)
+        {
+            if(name == musicClips[i].name)
+            {
+                audioSource.Play();
+            }
+        }
+        */
     }
 }
