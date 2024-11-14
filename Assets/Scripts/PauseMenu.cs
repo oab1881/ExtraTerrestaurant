7c1f9d71a0bcec889239cbaf11519712a3982e14
Owen Beck <112406398+oab1881@users.noreply.github.com>
2024-11-14 13:12:15 -0500

    Working on pause UI

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Written by Owen
+//This script allows player to open up the pause screen which will display some buttons and darken the backgroun
+public class PauseMenu : MonoBehaviour
+{ 
+    // Update is called once per frame
+    void Update()
+    {
+        //Track if player presses escape
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+        }
+    }
+}
