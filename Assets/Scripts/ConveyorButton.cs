e84cd9f02208551d1642743e4b4894e4cc6bc1db
Christopher Cabonilas <earthboundjesus77@gmail.com>
2024-11-14 20:27:55 -0500

    Conveyor movement and food deletion functional(?)

--- Assets/Scripts/ConveyorButton.cs
+++ Assets/Scripts/ConveyorButton.cs
@@ -18,28 +18,63 @@ public class ConveyorButton : MonoBehaviour
     [SerializeField]
     public ScoreManager scoring;
 
+    // Starting position: X:24.42, Y:1.14
+    // Conveyor position: X:15.3, Y:-3.5
+    [SerializeField]
+    GameObject tray;
+    [SerializeField]
+    GameObject newTray;
+
+    private Rigidbody2D rb2D;
+    private bool trayMoving = false;
+    private bool buttonActive = true;
+    private float conveyorSpeed = 0.5f;
+    private float time = 3.0f;
+
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rb2D = tray.GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
+        if (trayMoving)
+        {
+            rb2D.AddForce(-transform.up * conveyorSpeed);
+            time -= Time.deltaTime;
+
+            if (time < 0.0f)
+            {
+                trayMoving = false;
+                buttonActive = true;
+
+                rb2D.velocity = Vector3.zero;
+                scoring.DisplayScore();
+                Destroy(tray);
+                tray = Instantiate(newTray);
+                tray.transform.position = new Vector3(24.42f,1.14f, 0.0f);
+                ChangeSprite(unpressedButton);
+            }
+        }
 
     }
-
     private void OnMouseDown()
     {
-        ChangeSprite(pressedButton);
-        scoring.DisplayScore();
-        scoring.CleanPlate();
-    }
-    private void OnMouseUp()
-    {
-        ChangeSprite(unpressedButton);
+        if (buttonActive)
+        {
+            tray.transform.position = new Vector3(15.3f, -3.5f, 0.0f);
+            ChangeSprite(pressedButton);
+            MoveTray();
+        }
     }
 
     private void ChangeSprite(Sprite newSprite)
     {
         spriteRenderer.sprite = newSprite;
+    }
+    private void MoveTray()
+    {
+        trayMoving = true;
+        buttonActive = false;
     }
 }
